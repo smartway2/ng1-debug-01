c0fe2370688407b3b0987a903af305c08763faaf
John <jaboyak@gmail.com>
2017-05-09 07:53:47 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ng1-debug-01
+
+## Make it work!! There are a lot of bugs!
+
+## Guidelines:
+- Make all components in app.js show up on the page.
+- The form component must be functional!
---
+++ app.js
@@ -0,0 +1,39 @@
+var app = angular.model('ourApp', {});
+
+app.component('navBar', {
+  template: `
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  `
+})
+
+app.comonent('theForm', {
+  template: `
+    <p>Enter your name: <input type="text" ng-model="name"></p>
+    <p>Hello: {{name}}</p>
+  `
+  controller: function() {
+    var vm = this;
+  }
+})
